12a0cff11f4a1cf2956ea87e829feb964d4715b9
Ludwig Keyser <ludwig.keyser@gmail.com>
2022-06-19 20:20:15 +0100

    Added functionality that includes the date in the logfile name Added cleanup functions before the loop starts to ensure relay is open (off) and status messages are correct

--- temp.py
+++ temp.py
@@ -11,6 +11,7 @@ GPIO.setmode(GPIO.BCM) #Broadcom pin-numbering scheme
 GPIOChannel = 18
 
 GPIO.setup(GPIOChannel, GPIO.OUT) #set Relay 1 output
+GPIO.output(GPIOChannel,GPIO.LOW) #set Relay to open (off) for a clean start 
 
 spi = board.SPI()
 cs = digitalio.DigitalInOut(board.D5)
@@ -19,27 +20,32 @@ max31855 = adafruit_max31855.MAX31855(spi, cs)
 interval = 5
 targetTemp = 25.5
 variance = 1
-heatingIndicator = "off"
+heatingIndicator = "stable"
 
 while True:
+    # Read the current temperature
     tempNow = max31855.temperature
     
+    # Write the current temp to the logfile
+    # TODO: Add an option for the logfile name and location
     timeNow = datetime.now()
     timestampStr = timeNow.strftime("%d-%b-%Y (%H:%M:%S.%f)")
 
-    f = open('templog.csv', 'a')
+    f = open('templog-' + timeNow.strftime("%d-%b-%Y") + ".csv", 'a')
     f.write(timestampStr + ', ' + str(tempNow) + ', ' + heatingIndicator + '\n')
     f.close()
 
+    # If the measured temp is below the target temp + (1/2 the variance), turn the relay on
     if tempNow < targetTemp - (variance / 2):
-        if GPIO.input(GPIOChannel) == 0:
+        if GPIO.input(GPIOChannel) == 0: # Function returns 0 if relay is off
             GPIO.output(GPIOChannel, GPIO.HIGH) #turn heating on
-            heatingIndicator = "on"
+            heatingIndicator = "heating"
+    
     
     if tempNow >= (targetTemp + variance / 2):
-        if GPIO.input(GPIOChannel) == 1:
+        if GPIO.input(GPIOChannel) == 1: # Function returns 1 if relay is on
             GPIO.output(GPIOChannel, GPIO.LOW) #turn heating off
-            heatingIndicator = "off"
+            heatingIndicator = "stable"
 
     time.sleep(interval)
 
